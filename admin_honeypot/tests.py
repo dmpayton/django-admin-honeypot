15aaea344252e48ed0a7f2927a62ce6f60301c5b
Bruno Renié <buburno@gmail.com>
2011-09-27 14:57:16 +0200

    Enable the fake login screen for arbitrary URLs
    The Django admin display a login form for /admin/ but also for every child
    URL. URLs such as:
    
     * /admin/auth/user/1/
     * /admin/foobar/
    
    should not return a 404.
    
    Furthermore, every URL with no trailing slash issue a redirect with a
    trailing slash.

--- admin_honeypot/tests.py
+++ admin_honeypot/tests.py
@@ -4,6 +4,8 @@ from django.test import TestCase
 
 
 class AdminHoneypotTest(TestCase):
+    urls = 'admin_honeypot.urls'
+
     def test_admin_honeypot(self):
         data = {
             'username': 'admin',
@@ -13,3 +15,26 @@ class AdminHoneypotTest(TestCase):
         attempt = LoginAttempt.objects.latest('pk')
         self.assertEqual(data['username'], attempt.username)
         self.assertEqual(data['password'], attempt.password)
+
+    def test_arbitrary_urls(self):
+        """
+        The Django admin displays a login screen for everything under /admin/
+        """
+        data = {
+            'username': 'admin',
+            'password': 'letmein',
+        }
+        base_url = reverse('admin_honeypot')
+        for url in ('auth/', 'comments/moderate/', 'auth/user/1/'):
+            response = self.client.post(base_url + url, data)
+            attempt = LoginAttempt.objects.latest('pk')
+            self.assertEqual(data['username'], attempt.username)
+            self.assertEqual(data['password'], attempt.password)
+
+    def test_trailing_slash(self):
+        """
+        /admin/foo redirects to /admin/foo/
+        """
+        url = reverse('admin_honeypot')
+        response = self.client.get(url + 'foo')
+        self.assertRedirects(response, url + 'foo/')
--- admin_honeypot/views.py
+++ admin_honeypot/views.py
@@ -3,15 +3,19 @@ from admin_honeypot.models import LoginAttempt
 from admin_honeypot.signals import honeypot
 from django.contrib.auth import REDIRECT_FIELD_NAME
 from django.contrib.sites.models import Site
-from django.shortcuts import render_to_response
+from django.shortcuts import render_to_response, redirect
 from django.template import RequestContext
 from django.utils.translation import ugettext as _
 
 def admin_honeypot(request, extra_context=None):
+    path = request.get_full_path()
+    if not path.endswith('/'):
+        return redirect(path + '/')
+
     context = {
-        'app_path': request.get_full_path(),
+        'app_path': path,
         'form': HoneypotLoginForm(request, request.POST or None),
-        REDIRECT_FIELD_NAME: request.get_full_path(),
+        REDIRECT_FIELD_NAME: path,
         'site': Site.objects.get_current(),
         'title': _('Log in'),
         }
