e08fac61e5a581d2758f440df2b23608ea72f8f2
dmpayton <derek.payton@gmail.com>
2013-01-14 06:51:03 -0800

    Update urls for Django 1.5

--- admin_honeypot/urls.py
+++ admin_honeypot/urls.py
@@ -1,4 +1,7 @@
-from django.conf.urls.defaults import patterns, include, url
+try:
+    from django.conf.urls.defaults import patterns, include, url
+except ImportError:
+    from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns('admin_honeypot.views',
     url(r'^.*$', 'admin_honeypot', name='admin_honeypot'),
