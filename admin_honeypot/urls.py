2cffef9e225d49bf9b7237c324d3be6343e0517a
Matt Fisher <mrpfisher@gmail.com>
2013-10-01 12:47:29 +1000

    Change order of url imports to remove deprecation warning for Django 1.5

--- admin_honeypot/urls.py
+++ admin_honeypot/urls.py
@@ -1,7 +1,7 @@
 try:
-    from django.conf.urls.defaults import patterns, include, url
-except ImportError:
     from django.conf.urls import patterns, include, url
+except ImportError:
+    from django.conf.urls.defaults import patterns, include, url
 
 urlpatterns = patterns('admin_honeypot.views',
     url(r'^.*$', 'admin_honeypot', name='admin_honeypot'),
