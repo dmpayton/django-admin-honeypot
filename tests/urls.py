27b63d7386b71022794772deb12ae909ade0ed6b
Alexander <alexander@localhost.localdomain>
2013-10-25 16:51:29 -0500

    Changed the order of imports: "from django.conf.urls" to avoid DepreciationWarning.

--- tests/urls.py
+++ tests/urls.py
@@ -1,7 +1,7 @@
 try:
-    from django.conf.urls.defaults import patterns, include, url
-except ImportError:
     from django.conf.urls import patterns, include, url
+except ImportError:  # django < 1.4
+    from django.conf.urls.defaults import patterns, include, url
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
