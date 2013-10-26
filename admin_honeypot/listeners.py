d0fc22b215b369f3b2da4f5922346379105c05ab
dmpayton <derek.payton@gmail.com>
2013-10-25 18:13:54 -0700

    No longer require django.contrib.sites

--- admin_honeypot/listeners.py
+++ admin_honeypot/listeners.py
@@ -1,19 +1,16 @@
 from admin_honeypot.signals import honeypot
 from django.conf import settings
-from django.contrib.sites.models import get_current_site
 from django.core.mail import mail_admins
 from django.core.urlresolvers import reverse
 from django.template.loader import render_to_string
 
 
 def notify_admins(instance, request, **kwargs):
-    site = get_current_site(request)
     path = reverse('admin:admin_honeypot_loginattempt_change', args=(instance.pk,))
-    admin_detail_url = 'http://{0}{1}'.format(site.domain, path)
+    admin_detail_url = 'http://{0}{1}'.format(request.get_host(), path)
     context = {
         'request': request,
         'instance': instance,
-        'site': site,
         'admin_detail_url': admin_detail_url,
     }
     subject = render_to_string('admin_honeypot/email_subject.txt', context).strip()
--- tests/test_settings.py
+++ tests/test_settings.py
@@ -114,7 +114,7 @@ INSTALLED_APPS = (
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
-    'django.contrib.sites',
+    #'django.contrib.sites',
     'django.contrib.messages',
     #'django.contrib.staticfiles',
     'admin_honeypot',
