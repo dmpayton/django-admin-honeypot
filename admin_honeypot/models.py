d41a26bf36dd5ae6f7baf5ba8aa5fa11e4182f8a
Basil Shubin <basil.shubin@gmail.com>
2013-11-15 11:16:13 +0700

    add i18n support

--- admin_honeypot/models.py
+++ admin_honeypot/models.py
@@ -1,17 +1,20 @@
 from django.db import models
+from django.utils.translation import ugettext_lazy as _
 from admin_honeypot import listeners
 
 
 class LoginAttempt(models.Model):
-    username = models.CharField(max_length=255, blank=True, null=True)
-    password = models.CharField(max_length=255, blank=True, null=True)
-    ip_address = models.IPAddressField(blank=True, null=True)
-    session_key = models.CharField(max_length=50, blank=True, null=True)
-    user_agent = models.TextField(blank=True, null=True)
-    timestamp = models.DateTimeField(auto_now_add=True)
-    path = models.CharField(max_length=255, blank=True, null=True)
+    username = models.CharField(_("username"), max_length=255, blank=True, null=True)
+    password = models.CharField(_("password"), max_length=255, blank=True, null=True)
+    ip_address = models.IPAddressField(_("ip address"), blank=True, null=True)
+    session_key = models.CharField(_("session key"), max_length=50, blank=True, null=True)
+    user_agent = models.TextField(_("user-agent"), blank=True, null=True)
+    timestamp = models.DateTimeField(_("timestamp"), auto_now_add=True)
+    path = models.CharField(_("path"), max_length=255, blank=True, null=True)
 
     class Meta:
+        verbose_name = _("login attempt")
+        verbose_name_plural = _("login attempts")
         ordering = ('timestamp',)
 
     def __unicode__(self):
