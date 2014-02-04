3b4da1f2eae040542908f4802f54dd865de868d9
dmpayton <derek.payton@gmail.com>
2014-02-04 02:29:28 -0800

    Switch to py.test for tests.

---
+++ tests/settings.py
@@ -0,0 +1,42 @@
+import os
+
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+
+DEBUG = True
+TEMPLATE_DEBUG = DEBUG
+
+ADMINS = MANAGERS = (('Admin User', 'admin@example.com'))
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': ':memory:',
+    }
+}
+
+LANGUAGE_CODE = 'en-us'
+ROOT_URLCONF = 'tests.urls'
+SECRET_KEY = 'local'
+SITE_ID = 1
+TIME_ZONE = 'UTC'
+USE_I18N = True
+USE_L10N = True
+USE_TZ = True
+
+MEDIA_ROOT = os.path.join(PROJECT_ROOT, 'media')
+MEDIA_URL = '/media/'
+
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
+STATIC_URL = '/static/'
+
+INSTALLED_APPS = (
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+
+    'admin_honeypot',
+)
+
+ADMIN_HONEYPOT_EMAIL_ADMINS = True
