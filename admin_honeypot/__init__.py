5da47ea74a79fc71b3e78a673c4d1028bdb775b2
dmpayton <derek.payton@gmail.com>
2014-02-28 23:15:11 -0800

    Version bump: 0.3.0

--- admin_honeypot/__init__.py
+++ admin_honeypot/__init__.py
@@ -1,5 +1,5 @@
 __author__ = 'Derek Payton <derek.payton@gmail.com>'
 __copyright__ = 'Copyright (c) Derek Payton'
 __description__ = 'A fake Django admin login screen to notify admins of attempted unauthorized access.'
-__version__ = '0.2.6'
+__version__ = '0.3.0'
 __license__ = 'MIT License'
