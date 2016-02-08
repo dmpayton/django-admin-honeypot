3d6abacd8929256493505eafa8ffdfec501790a8
dmpayton <derek.payton@gmail.com>
2016-02-08 04:11:01 -0800

    Remove unused imports.

--- admin_honeypot/urls.py
+++ admin_honeypot/urls.py
@@ -1,5 +1,5 @@
 from admin_honeypot import views
-from django.conf.urls import patterns, include, url
+from django.conf.urls import url
 
 urlpatterns = [
     url(r'^login/$', views.AdminHoneypot.as_view(), name='login'),
