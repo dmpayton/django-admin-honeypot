58d51048d6c7086e8e7b235f8cebeed6524f5d00
dmpayton <derek.payton@gmail.com>
2016-02-08 04:08:30 -0800

    Remove usage of deprecated patterns() in urls. (Fixes #38)

--- admin_honeypot/urls.py
+++ admin_honeypot/urls.py
@@ -1,7 +1,7 @@
 from admin_honeypot import views
 from django.conf.urls import patterns, include, url
 
-urlpatterns = patterns('',
+urlpatterns = [
     url(r'^login/$', views.AdminHoneypot.as_view(), name='login'),
     url(r'^.*$', views.AdminHoneypot.as_view(), name='index'),
-)
+]
