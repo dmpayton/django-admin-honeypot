91924836483bfaa41cd9accfa2c27cf1e9bf0303
Gilles Dartiguelongue <g.dartiguelongue@lexfo.fr>
2017-11-13 12:59:10 +0100

    Update testsuite urls.py for target Django versions

--- tests/urls.py
+++ tests/urls.py
@@ -1,13 +1,10 @@
-try:
-    from django.conf.urls import patterns, include, url
-except ImportError:  # django < 1.4
-    from django.conf.urls.defaults import patterns, include, url
+from django.conf.urls import include, url
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
 admin.autodiscover()
 
-urlpatterns = patterns('',
+urlpatterns = [
     url(r'^admin/', include('admin_honeypot.urls', namespace='admin_honeypot')),
     url(r'^secret/', include(admin.site.urls)),
-)
+]
