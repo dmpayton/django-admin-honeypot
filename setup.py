5f536af9591df60eadb12ef2aaee1d4925229055
dmpayton <derek.payton@gmail.com>
2018-05-14 18:46:55 -0400

    Update trove classifiers.

--- setup.py
+++ setup.py
@@ -15,9 +15,8 @@ setup(
     classifiers=[
         'Development Status :: 5 - Production/Stable',
         'Framework :: Django',
-        'Framework :: Django :: 1.8',
-        'Framework :: Django :: 1.10',
         'Framework :: Django :: 1.11',
+        'Framework :: Django :: 2.0',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
         'Natural Language :: English',
