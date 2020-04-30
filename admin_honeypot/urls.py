86d12c7d13bd7a11a93deccf42f93df4328e70fd
dmpayton <derek.payton@gmail.com>
2020-04-30 03:56:03 -0700

    Update url() to path() in the urlconf.

--- admin_honeypot/urls.py
+++ admin_honeypot/urls.py
@@ -1,9 +1,9 @@
 from admin_honeypot import views
-from django.conf.urls import url
+from django.urls import path, re_path
 
 app_name = 'admin_honeypot'
 
 urlpatterns = [
-    url(r'^login/$', views.AdminHoneypot.as_view(), name='login'),
-    url(r'^.*$', views.AdminHoneypot.as_view(), name='index'),
+    path('login/', views.AdminHoneypot.as_view(), name='login'),
+    re_path(r'^.*$', views.AdminHoneypot.as_view(), name='index'),
 ]
