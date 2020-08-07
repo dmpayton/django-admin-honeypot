e34a0769ff2a98c4bf311fe4f46bf83820e51808
Ronny Vedrilla <ronny.vedrilla@ambient-innovation.com>
2020-08-07 10:25:39 +0200

    Added python and django versions to travis

--- setup.py
+++ setup.py
@@ -16,6 +16,7 @@ setup(
         'Framework :: Django',
         'Framework :: Django :: 2.2',
         'Framework :: Django :: 3.0',
+        'Framework :: Django :: 3.1',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
         'Natural Language :: English',
