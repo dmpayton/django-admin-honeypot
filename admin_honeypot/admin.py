b3ff2b0183edfe5399723042b83632c4a800529d
dmpayton <derek.payton@gmail.com>
2021-12-31 04:48:33 -0800

    s/ugettext/gettext/g

--- admin_honeypot/admin.py
+++ admin_honeypot/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.utils.html import format_html
-from django.utils.translation import ugettext_lazy as _
+from django.utils.translation import gettext_lazy as _
 
 from admin_honeypot.models import LoginAttempt
 
